7a11871ff36cda4a41ae4c66a8acf9ecc8bd0c2c
LAMBERT\lambert <fuxiaolei@sinoac.org>
2018-07-19 11:59:59 +0800

    水纹效果

---
+++ and_ui_waveheader/app/src/main/java/com/scwang/wave/app/activity/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.scwang.wave.app.activity
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.scwang.wave.app.R
+import com.scwang.wave.app.fragment.WavePairFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.reflect.KClass
+
+class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+
+    enum class Tabs(val menuId: Int, val clazz: KClass<out Fragment>) {
+        WavePair(R.id.navigation_home, WavePairFragment::class),
+        Wave(R.id.navigation_dashboard, WavePairFragment::class),
+        Wave2(R.id.navigation_notifications, WavePairFragment::class),
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(this)
+        navigation.selectedItemId = R.id.navigation_home
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        supportFragmentManager
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(R.id.content, Tabs.values().first { it.menuId==item.itemId }.clazz.java.newInstance())
+                .commit()
+        return true
+    }
+
+}
---
+++ and_ui_waveheader/app/src/main/java/com/scwang/wave/app/activity/UserLoginActivity.kt
@@ -0,0 +1,25 @@
+package com.scwang.wave.app.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.scwang.wave.app.R
+import com.scwang.wave.app.fragment.WavePairFragment
+import com.scwang.wave.app.util.StatusBarUtil
+import kotlinx.android.synthetic.main.activity_user_login.*
+
+
+class UserLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_login)
+        //状态栏透明和间距处理
+        StatusBarUtil.immersive(this)
+
+        login.setOnClickListener {
+//            startActivity(Intent(this, MainActivity::class.java))
+            FragmentActivity.start(this, WavePairFragment::class.java)
+        }
+
+    }
+}
